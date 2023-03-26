4b733fa3b45536de4e1ad04b467f2e821b7355dd
oauguste <augusteosnac@gmail.com>
2023-03-26 13:16:15 -0400

    done

--- Weathers/src/App.jsx
+++ Weathers/src/App.jsx
@@ -4,7 +4,6 @@ import WeatherCard from "./components/WeatherCard";
 function App() {
   return (
     <div className="App">
-      <h1>Hello this is a weather app</h1>
       <WeatherCard />
     </div>
   );
